2f88214b7023a8c69d4092490dc39c291349ab5b
Leonardocasi <leocasillasg@gmail.com>
2026-03-17 00:22:09 -0600

    Creación

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+// Inizialización del entorno.
+const lienzo = document.getElementById("game")
+const ctx = lienzo.getContext("2d") 			//Abreviación de contexto
+
+lienzo.width = innerWidth
+lienzo.height = innerHeight
+
